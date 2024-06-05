78d40c86c1773b92fc7a1b69af38cf8e4c82e578
Yosselyn Hernandez <yosselynhernande19-25@gmail.com>
2024-06-04 20:47:29 -0600

    Avance1

---
+++ JuegoPiedraPapelTijeraDEMO/JuegoPiedraPapelTijera/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Media;
+using System.Media;
+namespace JuegoPiedraPapelTijera
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InterfazUsuario interfaz = new InterfazUsuario();
+            interfaz.MostrarMenuPrincipal();
+
+        }
+
+    }
+}
